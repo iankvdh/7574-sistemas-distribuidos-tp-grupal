711bbe85df131d5094da4fde6f405f2572028835
iankvdh <ivon@fi.uba.ar>
2026-05-17 20:25:16 -0300

    feat (batch size): se agregó nueva variable de entorno BATCH_MAX_BYTES para configurar el máximo tamaño de un batch

--- src/client/client/transactions.go
+++ src/client/client/transactions.go
@@ -11,6 +11,10 @@ import (
 	"github.com/iankvdh/7574-sistemas-distribuidos-tp-grupal/common/transaction"
 )
 
+const (
+	transactionBatchHeaderBytes = 1 + 4 // MsgType + uint32 batch size
+)
+
 func (client *Client) sendTransactions() error {
 	file, err := os.Open(client.config.InputTransactions)
 	if err != nil {
@@ -25,7 +29,8 @@ func (client *Client) sendTransactions() error {
 		return fmt.Errorf("reading transactions header: %w", err)
 	}
 
-	batch := make([]transaction.Transaction, 0, client.config.BatchSize)
+	batch := make([]transaction.Transaction, 0, 16)
+	batchBytes := transactionBatchHeaderBytes
 	for {
 		row, err := reader.Read()
 		if errors.Is(err, io.EOF) {
@@ -39,14 +44,27 @@ func (client *Client) sendTransactions() error {
 		if err != nil {
 			return fmt.Errorf("parsing transaction row: %w", err)
 		}
-		batch = append(batch, tx)
 
-		if len(batch) >= client.config.BatchSize {
+		txBytes := transactionSize(tx)
+		if txBytes+transactionBatchHeaderBytes > client.config.BatchMaxBytes {
+			return fmt.Errorf(
+				"transaction record too large for BATCH_MAX_BYTES: record=%d header=%d max=%d",
+				txBytes,
+				transactionBatchHeaderBytes,
+				client.config.BatchMaxBytes,
+			)
+		}
+
+		if len(batch) > 0 && batchBytes+txBytes > client.config.BatchMaxBytes {
 			if err := client.flushTransactionBatch(batch); err != nil {
 				return err
 			}
 			batch = batch[:0]
+			batchBytes = transactionBatchHeaderBytes
 		}
+
+		batch = append(batch, tx)
+		batchBytes += txBytes
 	}
 
 	if len(batch) > 0 {
@@ -66,4 +84,15 @@ func (client *Client) flushTransactionBatch(batch []transaction.Transaction) err
 		return err
 	}
 	return client.expectMsgType(external.Ack)
+}
+
+func transactionSize(tx transaction.Transaction) int {
+	return 4 + // Date
+		4 + // FromBank
+		1 + len(tx.FromAccount) +
+		4 + // ToBank
+		1 + len(tx.ToAccount) +
+		8 + // AmountPaidCents
+		1 + len(tx.PaymentCurrency) +
+		1 + len(tx.PaymentFormat)
 }
