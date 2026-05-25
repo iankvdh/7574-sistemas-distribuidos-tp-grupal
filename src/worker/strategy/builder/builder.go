0b67202a0674dd2dc3a0f8b74230cbfbc7537e89
AgustinAltamirano <agustinga110704@gmail.com>
2026-05-24 23:19:13 -0300

    refactor(worker): se agrega suspiciuous filter a la q4 para mejorar el uso de memoria

--- src/worker/strategy/builder/builder.go
+++ src/worker/strategy/builder/builder.go
@@ -7,13 +7,13 @@ import (
 	"github.com/iankvdh/7574-sistemas-distribuidos-tp-grupal/worker/strategy/counter"
 	"github.com/iankvdh/7574-sistemas-distribuidos-tp-grupal/worker/strategy/drain"
 	"github.com/iankvdh/7574-sistemas-distribuidos-tp-grupal/worker/strategy/filter"
+	"github.com/iankvdh/7574-sistemas-distribuidos-tp-grupal/worker/strategy/finaljoiner"
 	"github.com/iankvdh/7574-sistemas-distribuidos-tp-grupal/worker/strategy/joiner"
 	"github.com/iankvdh/7574-sistemas-distribuidos-tp-grupal/worker/strategy/noop"
-	"github.com/iankvdh/7574-sistemas-distribuidos-tp-grupal/worker/strategy/finaljoiner"
 	"github.com/iankvdh/7574-sistemas-distribuidos-tp-grupal/worker/strategy/pathfinder"
-	"github.com/iankvdh/7574-sistemas-distribuidos-tp-grupal/worker/strategy/scatteragg"
 	"github.com/iankvdh/7574-sistemas-distribuidos-tp-grupal/worker/strategy/sharder"
 	sharder_q1 "github.com/iankvdh/7574-sistemas-distribuidos-tp-grupal/worker/strategy/sharder_q1"
+	"github.com/iankvdh/7574-sistemas-distribuidos-tp-grupal/worker/strategy/suspicious_filter"
 )
 
 func Build(name string) (strategy.Strategy, error) {
@@ -55,14 +55,14 @@ func Build(name string) (strategy.Strategy, error) {
 	case "filter_currency_usd_p1", "filter_currency_usd_p2", "filter_currency_usd_other_periods":
 		return filter.New(name, filter.IsUSDPredicate()).
 			WithMatchProjection(filter.WithoutPaymentCurrency), nil
-	case "scatter_agg_q4":
-		return scatteragg.New(), nil
 	case "sharder_q4":
 		return sharder.New(), nil
 	case "sharder_q1":
 		return sharder_q1.New(), nil
 	case "final_joiner":
 		return finaljoiner.New(), nil
+	case "suspicious_account_filter":
+		return suspicious_filter.New(), nil
 	case "path_finder_q4":
 		return pathfinder.New(), nil
 	case "counter_q4":
